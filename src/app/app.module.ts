465f901e6f8521b5bbcba47dfe0f1a5380d3e992
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-04-01 11:57:18 +0300

    added component required for the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
+import { AddProjectComponent } from './components/add-project/add-project.component';
+import { SingleProjectComponent } from './components/single-project/single-project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    LandingComponent,
+    ProfileComponent,
+    EditProfileComponent,
+    AddProjectComponent,
+    SingleProjectComponent
   ],
   imports: [
     BrowserModule,

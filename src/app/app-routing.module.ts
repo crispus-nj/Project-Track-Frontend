955ece15f0312f2179f100d959d21031b2dd5e57
PapaFreddie <fred.papa@student.moringaschool.com>
2022-04-02 14:50:23 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProjectComponent } from './components/add-project/add-project.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
  {
   path: 'profile',
   component: ProfileComponent
+ },
+ {
+   path: 'add-project', component: AddProjectComponent
  },
 ];
 

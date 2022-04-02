447713d9be50a56752860b7541afc8cb42fd824d
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-04-02 14:44:08 +0300

    create a project interface in the projects.models.ts, added the rules for a specific project, created an array of project to be looped through, looped through the array and diplayed all projects in the projects html template

---
+++ src/app/components/projects/projects.model.ts
@@ -0,0 +1,9 @@
+export interface Project {
+    owner: string,
+    project_name: string,
+    category: string,
+    description: string,
+    repo_link: string,
+    technologies: string,
+    image: string
+}

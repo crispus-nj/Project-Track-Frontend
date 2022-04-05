96301f87f2718b312b2b33a70b7fd9f3b1de4dfd
PapaFreddie <fred.papa@student.moringaschool.com>
2022-04-05 19:03:34 +0300

    form validation

--- src/app/components/add-project/add-project.component.ts
+++ src/app/components/add-project/add-project.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { OpaqueValue } from '@angular/compiler/src/compiler_facade_interface';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-add-project',
   templateUrl: './add-project.component.html',
   styleUrls: ['./add-project.component.css']
 })
-export class AddProjectComponent implements OnInit {
+export class AddProjectComponent implements OnInit { 
+  text!: string;
+  file!: OpaqueValue;
+
+description:string = ''
+@ViewChild('form') addproject: ElementRef
 
   constructor() { }
 
+
   ngOnInit(): void {
   }
+
+  onSubmit(){
+    console.log(this.addproject)
+    if(!this.text){
+      alert('Please add project!!');
+      return;
+    }
+  }
+
 
 }

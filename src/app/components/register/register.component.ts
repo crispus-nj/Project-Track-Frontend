e1f8b0e2337c72dd7bdfe5e7d86cdd4be42be92b
trevin-livele <liveletrevin6@gmail.com>
2022-04-06 18:14:44 +0300

    added edit profile

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Register } from 'src/app/register';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,14 +8,17 @@ import { Register } from 'src/app/register';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+@ViewChild('registerForm') signup : NgForm;
 registerModel = new Register('trevin', 'trevin@gmail.com', 'trevin', 'trevin');
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
 onSubmit(){
-  console.log('registerModel')
+  console.log(this.signup.value.password,
+    this.signup.value.email,this.signup.value.confirmPassword,this.signup.value.username);
+    this.signup.reset()
 }
 }

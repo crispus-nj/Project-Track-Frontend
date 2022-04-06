478bc648c59c04bb385cdeb8bb6ba8783fde2126
trevin-livele <liveletrevin6@gmail.com>
2022-04-06 14:44:03 +0300

    added clases for auth data

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Register } from 'src/app/register';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+registerModel = new Register('trevin', 'trevin@gmail.com', 'trevin', 'trevin');
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+onSubmit(){
+  console.log('registerModel')
+}
 }

f6086d2ce41b739d4db5914a3bc0a552c76d401f
PapaFreddie <fred.papa@student.moringaschool.com>
2022-04-15 12:43:36 +0300

    modified a add project component

--- src/app/components/add-project/add-project.component.ts
+++ src/app/components/add-project/add-project.component.ts
@@ -1,6 +1,8 @@
 import { OpaqueValue } from '@angular/compiler/src/compiler_facade_interface';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Stack } from './stack.model';
 
 @Component({
   selector: 'app-add-project',
@@ -11,18 +13,38 @@ export class AddProjectComponent implements OnInit {
   text!: string;
   file!: OpaqueValue;
 
+stack: Stack[] = []
+
 description:string = ''
 @ViewChild('form') addproject: NgForm
 
-  constructor() { }
+  constructor(private route:Router, private httpService: ProjectsService
+    ) { }
 
 
   ngOnInit(): void {
   }
 
   onSubmit(){
-    console.log(this.addproject.value.name, this.addproject.value.member, this.addproject.value.link, this.addproject.value.file)
+   const name = this.addproject.value.name
+   const description = this.addproject.value.description
+   const image = this.addproject.value.image
+   const github_link = this.addproject.value.github_link
+   const track_id = this.addproject.value.track.id
+   const technologies = this.addproject.value.technologies
+    // console.log("jina",name, "Description",description, "picha",image, "link",github_link, "tracks",track, "tech",technologies)
+    this.httpService.postProjects(name, description, image, track_id, technologies, github_link).subscribe(res=> {
+      this.route.navigate(['/'])
+    })
     this.addproject.reset()
+  }
+
+  getStacks(){
+    this.httpService.getStack().subscribe(data => {
+      // console.log((data)) 
+      this.stack = Object.values(data)
+      console.log(this.stack)
+    })
   }
 
 
